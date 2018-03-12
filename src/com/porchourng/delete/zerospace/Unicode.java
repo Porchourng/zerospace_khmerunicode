461778a0fba51f78a4770c947b5b9fde4ddbae67
porchourng <porchourng@gmail.com>
2018-03-12 09:47:07 +0700

    optimize function clean Unicode

--- src/com/porchourng/delete/zerospace/Unicode.java
+++ src/com/porchourng/delete/zerospace/Unicode.java
@@ -4,50 +4,30 @@ import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
 
 public class Unicode {
-    // or "ISO-8859-1" for ISO Latin 1
-    static CharsetEncoder asciiEncoder = Charset.forName("US-ASCII").newEncoder();
 
     public static boolean isPureAscii(char v) {
-        boolean b = asciiEncoder.canEncode(v);
-//        if (!b) {
-//            System.out.println("asscii : "+b+ ", char "+v);
-//        }
-
-        return b;
+        // or "ISO-8859-1" for ISO Latin 1
+        CharsetEncoder asciiEncoder = Charset.forName("US-ASCII").newEncoder();
+        return asciiEncoder.canEncode(v);
     }
 
-    public static void main(String[] args) {
-        // increment character
-        //char ch = 'a';
-        //System.out.println(++ch);
-
-        // Khmer Unicode zero space
-        char zerospace = '​';
-        System.out.println((int)zerospace); // convert char to Unicode
-
-        char unicodeToChar = '\u0041';
-        System.out.println(unicodeToChar);
-
-        String str = "រាប់ចាប់​ពី​ការងារ​កាត់ស្មៅ ​និង ​ជូតកញ្ចក់​ ដល់​ការងារ​នាំ​ឥវ៉ាន់​ចែក​ចាយ​នៅ​ឯ​មន្ទីរពេទ្យ​និងសណ្ឋាគារ ​ឬ​ក៏មើល​ថែ​កូន​ក្មេង ​ហើយនឹង​មនុស្ស​ចាស់ ​មនុស្ស យន្ត​គ្រប់​ទំហំ​និង​បែប​សណ្ឋាន​អាចធ្វើ​កិច្ចការ​ដែល​មនុស្ស​ធ្វើ​ជា​ច្រើន​ឆ្នាំ​ទាំង នោះ​បាន។​ ម៉ាស៊ីន​ទាំង​នេះ​អាចធ្វើ​កិច្ចការ​ដែល​មនុស្ស​មិន​អាច​ធ្វើ​បាន​ផង​ក៏​មាន។\n" +
-                "\n" +
-                "លោក ​ស៊ីម៉ូន វ៉ាង ​(Simon Wang) ​នៃ​ក្រុមហ៊ុន ​Beijing Canny ​Unisboro Technology ​Co. Ltd បាន​ថ្លែងថា៖\n" +
-                "\n" +
-                "«មនុស្សយន្ត​អាច​ធ្វើ​ការ​ពេញ​មួយ​ថ្ងៃ​ ២៤ម៉ោង​ក្នុង​មួយ​ថ្ងៃ​ ហើយ​គ្មាន​អារម្មណ៍ ធុញទ្រាន់​ទេ។​ ជានិច្ច​ជា​កាល ​វា​ស្វាគមន៍​ដោយ​ភាព​ញញឹម‍»។";
+    /*
+        This function use for clean zero space in Khmer Unicode and ASCII code
+        from Khmer sentence Unicode.
+     */
+    public static String cleanKhmerStr(String str) {
+        char zerospace = '\u8203';
         StringBuilder sb = new StringBuilder(str);
         StringBuilder sbNew = new StringBuilder();
-
         for(int i=0; i<sb.length(); i++) {
-
-            if (sb.charAt(i) == zerospace || Unicode.isPureAscii(sb.charAt(i))) {
-
-            } else {
+            if (sb.charAt(i) != zerospace && !Unicode.isPureAscii(sb.charAt(i))) {
                 sbNew.append(sb.charAt(i));
             }
         }
+        return sbNew.toString();
+    }
 
-        System.out.print(sbNew.toString());
-
-
-
+    public static int convertUnicodeToInt(char c) {
+        return (int)c;
     }
 }
